a87f47e432426139495a3d5d1ffe08b5e9db1001
Vikash <vikash.gupta@visiblealpha.com>
2017-02-17 14:56:49 +0530

    code modularize

--- consul_contents/consul.py
+++ consul_contents/consul.py
@@ -1,33 +1,42 @@
 import consulate
 import os
 
-#
-# CONSUL_CONFIG = dict()
-CONSUL_NAME = os.environ.get('CONSUL_NAME', 'dev')
 
-consul = consulate.Consul(host='localhost', port=8500)
-config = consul.kv.find(CONSUL_NAME)
+CONSUL_ENV_NAME = 'CONSUL_NAME'
+DEFAULT_CONSUL_NAME = 'default'
 
 
-def assign_consul(data, key, split_keys, start, end, CONSUL_CONFIG):
-    if start > end or split_keys[start] == '':
-        return
-    if start == end:
-        CONSUL_CONFIG[split_keys[start]] = str(data[key])
-    else:
-        if split_keys[start] not in CONSUL_CONFIG:
-            CONSUL_CONFIG[split_keys[start]] = {}
-        assign_consul(data, key, split_keys, start+1, end, CONSUL_CONFIG[split_keys[start]])
+class ConsulContents:
+    def __init__(self, consul_server):
+        self.consul_name = os.environ.get('CONSUL_ENV_NAME', None)
+        self.consul = consulate.Consul(host=consul_server['host'], port=consul_server['port'])
+        self.consul_key_value_default = self.consul.kv.find(DEFAULT_CONSUL_NAME)
+        self.consul_key_value_env = None
+        if self.consul_name:
+            self.consul_key_value_env = self.consul.kv.find(self.consul_name)
+        self.consul_nested_data = dict()
 
+    def assign_consul(self, data, key, split_keys, start, end, consul_nested_data):
+        if start > end or split_keys[start] == '':
+            return
+        if start == end:
+            consul_nested_data[split_keys[start]] = str(data[key])
+        else:
+            if split_keys[start] not in consul_nested_data:
+                consul_nested_data[split_keys[start]] = dict()
+            self.assign_consul(data, key, split_keys, start+1, end, consul_nested_data[split_keys[start]])
 
-CONSUL_CONFIG = dict()
-for key in config:
-    split_keys = str(key).split('/')
-    l = len(split_keys)
-    assign_consul(config, key, split_keys, 1, l-1, CONSUL_CONFIG)
-    # print CONSUL_CONFIG
+    def set_consul_nested_data(self, datasets):
+        for key in datasets:
+            split_keys = str(key).split('/')
+            keys_len = len(split_keys)
+            self.assign_consul(datasets, key, split_keys, 1, keys_len-1, self.consul_nested_data)
 
-print CONSUL_CONFIG
+    def get_consul_data(self):
+        self.set_consul_nested_data(self.consul_key_value_default)
+        if self.consul_key_value_env:
+            self.set_consul_nested_data(self.consul_key_value_env)
+        return self.consul_nested_data
 
 
 # consulate --datacenter dc1 --token 33921f61-d6b9-6c89-a6bb-6c872c33f83c kv backup > sample.json
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='consul-contents',
-      version='0.1',
+      version='0.1.1',
       description='This is for loading key-value stored on consul server',
       url='https://github.com/vidur-88/consul_contents.git',
       author='vikash gupta',
       author_email='vgupta840@gmail.com',
       license='',
+      install_requires=['consulate==0.6.0'],
       packages=['consul_contents'],
       zip_safe=False)
