f9fcc633d83f7621fd9f1a49096ce4c87fa877f5
Vikash <vikash.gupta@visiblealpha.com>
2017-04-19 21:30:47 +0530

    change consul params for token

--- consul_contents/consul.py
+++ consul_contents/consul.py
@@ -9,7 +9,12 @@ DEFAULT_CONSUL_NAME = 'default'
 class ConsulContents:
     def __init__(self, consul_server):
         self.consul_name = os.environ.get('CONSUL_ENV_NAME', None)
-        self.consul = consulate.Consul(host=consul_server['host'], port=consul_server['port'])
+        self.consul = consulate.Consul(host=consul_server['host'],
+                                       port=consul_server['port'],
+                                       token=consul_server.get('token'),
+                                       datacenter=consul_server.get('datacenter'),
+                                       scheme=consul_server.get('scheme', 'http'),
+                                       adapter=consul_server.get('adapter'))
         self.consul_key_value_default = self.consul.kv.find(DEFAULT_CONSUL_NAME)
         self.consul_key_value_env = None
         if self.consul_name:
@@ -37,7 +42,3 @@ class ConsulContents:
         if self.consul_key_value_env:
             self.set_consul_nested_data(self.consul_key_value_env)
         return self.consul_nested_data
-
-
-# consulate --datacenter dc1 --token 33921f61-d6b9-6c89-a6bb-6c872c33f83c kv backup > sample.json
-# cat sample.json | consulate --datacenter dc1 --token 33921f61-d6b9-6c89-a6bb-6c872c33f83c kv restore
